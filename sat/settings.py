f8c43371fb2517501963e8e695fbd5fa769b0cfe
Derek Shoemaker <shoemakerdr@gmail.com>
2018-01-26 22:50:57 -0600

    Added MEDIA_ROOT and MEDIA_URL for serving images

--- sat/settings.py
+++ sat/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'floorplans'
+    'floorplans',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.abspath('sat/media')
+
+MEDIA_URL = '/media/'
