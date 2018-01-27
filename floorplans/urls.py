87e12f7a0ff963c35e71eba8224aceb14e3542a1
Derek Shoemaker <shoemakerdr@gmail.com>
2018-01-27 17:36:41 -0600

    Added url patterns for index, login, logout, viewing floorplans, and editing floorplans.

--- floorplans/urls.py
+++ floorplans/urls.py
@@ -1,8 +1,21 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 
-from . import views
+from .views import index, dashboard, view_floorplan, edit_floorplan
 
 urlpatterns = [
-    #  path('', views.index, name='floorplans_dashboard'),
-    path('dashboard/', views.dashboard, name='floorplans_dashboard'),
+    path('', index, name='floorplans_index'),
+    path('dashboard/', dashboard, name='floorplans_dashboard'),
+    path('login/',
+         LoginView.as_view(template_name='floorplans/login_form.html'),
+         name='floorplans_login'),
+    path('logout',
+         LogoutView.as_view(),
+         name='floorplans_logout'),
+    path('floorplan/view/<int:floorplan_id>',
+         view_floorplan,
+         name='floorplans_view'),
+    path('floorplan/edit/<int:floorplan_id>',
+         edit_floorplan,
+         name='floorplans_edit'),
 ]
