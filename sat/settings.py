43d4e8ea872c2b32f1b8d772ad4b675fda98046e
Derek Shoemaker <shoemakerdr@gmail.com>
2018-01-27 17:35:48 -0600

    Added settings for top level static and template files. Added url settings for login/logout views

--- sat/settings.py
+++ sat/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'sat.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+LOGIN_REDIRECT_URL = 'floorplans_dashboard'
+
+LOGOUT_REDIRECT_URL = 'floorplans_index'
+
+LOGIN_URL = 'floorplans_login'
 
 MEDIA_ROOT = os.path.abspath('sat/media')
 
