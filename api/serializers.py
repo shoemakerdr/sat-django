2495149c6cf81aea8810659b1543a14ebf4f9a69
Derek Shoemaker <shoemakerdr@gmail.com>
2018-02-05 22:49:44 -0600

    Created serializer for User. Now has nested serializers for floorplans in User and locations in FloorPlan

--- api/serializers.py
+++ api/serializers.py
@@ -1,14 +1,25 @@
 from rest_framework import serializers
-from floorplans.models import User, FloorPlan, Location, LOCATION_TYPES
-
-class FloorPlanSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = FloorPlan
-        fields = ('id', 'name', 'image', 'width_ratio', 'height_ratio', 'last_updated')
-
+from floorplans.models import User, FloorPlan, Location
+from django.contrib.auth.models import User
 
 class LocationSerializer(serializers.ModelSerializer):
     class Meta:
-        model = FloorPlan
+        model = Location
         fields = ('id', 'name', 'loc_type', 'details', 'extension', 'position_x', 'position_y', 'last_updated')
+
+
+class FloorPlanSerializer(serializers.ModelSerializer):
+    locations = LocationSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = FloorPlan
+        fields = ('id', 'user', 'name', 'image', 'width_ratio', 'height_ratio', 'locations', 'last_updated')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    floorplans = FloorPlanSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'floorplans')
 
