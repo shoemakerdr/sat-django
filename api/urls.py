7678339f740f03a5aef8af9e1c06218ca36693d6
Derek Shoemaker <shoemakerdr@gmail.com>
2018-02-08 14:02:34 -0600

    Changed view functions to class based api views in url patterns

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('floorplans/', views.floorplan_list),
-    re_path('floorplans/(?P<pk>[0-9]+)/$', views.floorplan_detail),
+    path('floorplans/', views.FloorPlanList.as_view()),
+    re_path('floorplans/(?P<pk>[0-9]+)/$', views.FloorPlanDetail.as_view()),
 ]
