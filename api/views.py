2500d3bd65b678c1006e55002c485b3069e348b2
Derek Shoemaker <shoemakerdr@gmail.com>
2018-02-08 14:01:30 -0600

    Replaced function views with class-based api views.

--- api/views.py
+++ api/views.py
@@ -1,50 +1,13 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
 from floorplans.models import FloorPlan
 from .serializers import FloorPlanSerializer
+from rest_framework import mixins
+from rest_framework import generics
 
-@csrf_exempt
-def floorplan_list(request):
-    """
-    List all code floorplans, or create a new floorplan.
-    """
-    if request.method == 'GET':
-        floorplans = FloorPlan.objects.all()
-        serializer = FloorPlanSerializer(floorplans, many=True)
-        return JsonResponse(serializer.data, safe=False)
+class FloorPlanList(generics.ListCreateAPIView):
+    queryset = FloorPlan.objects.all()
+    serializer_class = FloorPlanSerializer
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = FloorPlanSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
 
-@csrf_exempt
-def floorplan_detail(request, pk):
-    """
-    Retrieve, update or delete a code floorplan.
-    """
-    try:
-        floorplan = FloorPlan.objects.get(pk=pk)
-    except FloorPlan.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = FloorPlanSerializer(floorplan)
-        return JsonResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = FloorPlanSerializer(floorplan, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        floorplan.delete()
-        return HttpResponse(status=204)
+class FloorPlanDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = FloorPlan.objects.all()
+    serializer_class = FloorPlanSerializer
