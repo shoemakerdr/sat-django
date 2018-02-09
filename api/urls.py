9bc5b3713930917d0ecae576928c9e3ad4878023
Derek Shoemaker <shoemakerdr@gmail.com>
2018-02-09 13:36:39 -0600

    Added location views and url patterns.

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('floorplans/', views.FloorPlanList.as_view()),
     re_path('floorplans/(?P<pk>[0-9]+)/$', views.FloorPlanDetail.as_view()),
+    path('locations/', views.LocationList.as_view()),
+    re_path('locations/(?P<pk>[0-9]+)/$', views.LocationDetail.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,5 @@
-from floorplans.models import FloorPlan
-from .serializers import FloorPlanSerializer
-from rest_framework import mixins
+from floorplans.models import FloorPlan, Location
+from .serializers import FloorPlanSerializer, LocationSerializer
 from rest_framework import generics
 
 class FloorPlanList(generics.ListCreateAPIView):
@@ -11,3 +10,13 @@ class FloorPlanList(generics.ListCreateAPIView):
 class FloorPlanDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = FloorPlan.objects.all()
     serializer_class = FloorPlanSerializer
+
+
+class LocationList(generics.ListCreateAPIView):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+
+
+class LocationDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
