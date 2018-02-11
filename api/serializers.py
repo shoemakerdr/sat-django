d4eec9d1e550f7f1ef31970a0e5f617717de335c
Derek Shoemaker <shoemakerdr@gmail.com>
2018-02-11 17:45:15 -0600

    Added serializers for batch actions on locations

--- api/serializers.py
+++ api/serializers.py
@@ -2,10 +2,67 @@ from rest_framework import serializers
 from floorplans.models import User, FloorPlan, Location
 from django.contrib.auth.models import User
 
-class LocationSerializer(serializers.ModelSerializer):
+
+class LocationsByFloorPlanListSerializer(serializers.ListSerializer):
+    def update(self, instances, validated_data):
+        # Maps for id->location instance and id->data item.
+        location_mapping = {location.id: location for location in instances}
+        data_mapping = {item['id']: item for item in validated_data}
+
+        ret = []
+        for location_id, data in data_mapping.items():
+            location = location_mapping.get(location_id, None)
+            if location is not None:
+                ret.append(self.child.update(location, data))
+
+        return ret
+
+
+class LocationsByFloorPlanSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
+
     class Meta:
         model = Location
-        fields = ('id', 'name', 'loc_type', 'details', 'extension', 'position_x', 'position_y', 'last_updated')
+        list_serializer_class = LocationsByFloorPlanListSerializer
+        fields = (
+            'id',
+            'name',
+            'floorplan',
+            'loc_type',
+            'details',
+            'extension',
+            'position_x',
+            'position_y',
+            'is_trashed',
+            'last_updated'
+        )
+
+class LocationListSerializer(serializers.ListSerializer):
+    def create(self, validated_data):
+        ret = []
+        for location in validated_data:
+            ret.append(self.child.create(location))
+
+        return ret
+
+
+class LocationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Location
+        list_serializer_class = LocationListSerializer
+        fields = (
+            'id',
+            'name',
+            'floorplan',
+            'loc_type',
+            'details',
+            'extension',
+            'position_x',
+            'position_y',
+            'is_trashed',
+            'last_updated'
+        )
 
 
 class FloorPlanSerializer(serializers.ModelSerializer):
@@ -14,7 +71,16 @@ class FloorPlanSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = FloorPlan
-        fields = ('id', 'user', 'name', 'image', 'aspect_ratio', 'locations', 'last_updated')
+        fields = (
+            'id', 
+            'user', 
+            'name', 
+            'image', 
+            'aspect_ratio', 
+            'locations', 
+            'is_trashed',
+            'last_updated'
+        )
 
 
 class UserSerializer(serializers.ModelSerializer):
