64ce12ff44b9dd5aa1ba1eca83664d1e24aeeada
Derek Shoemaker <shoemakerdr@gmail.com>
2018-02-13 18:34:32 -0600

    Added view names and comments

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,18 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    re_path('floorplans/(?P<pk>[0-9]+)/$', views.FloorPlanDetail.as_view()),
-    re_path('floorplans/(?P<pk>[0-9]+)/locations/$', views.LocationsByFloorPlan.as_view()),
+    ####
+    # floorplans/<pk>/ ->
+    #   get FloorPlan instance and list of its Locations
+    ####
+    re_path('floorplans/(?P<pk>[0-9]+)/$',
+            views.FloorPlanDetail.as_view(),
+            name='api-floorplan'),
+    ####
+    # floorplans/<pk>/locations/ ->
+    #   get, create, update list of Locations for FloorPlan instance
+    ####
+    re_path('floorplans/(?P<pk>[0-9]+)/locations/$',
+            views.LocationsByFloorPlan.as_view(),
+            name='api-locations'),
 ]
