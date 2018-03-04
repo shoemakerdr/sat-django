356cee23eed990074cc5dcbbddb3d532cefbcaa8
Derek Shoemaker <shoemakerdr@gmail.com>
2018-03-04 13:20:33 -0600

    Added functions to map location type abbreviations to readable and back.

--- static/elm/src/FloorPlanTypes.elm
+++ static/elm/src/FloorPlanTypes.elm
@@ -3,7 +3,13 @@ module FloorPlanTypes
         ( FloorPlan
         , Location
         , newFloorPlan
+        , locMapAbbrToReadable
+        , locMapReadableToAbbr
+        , getLocationFromAbbr
+        , getLocationFromReadable
         )
+
+import Dict exposing (Dict, fromList, get)
 
 
 type alias FloorPlan =
@@ -47,3 +53,50 @@ newFloorPlan flag =
     , owner = flag.owner
     , last_updated = flag.last_updated
     }
+
+
+locMapAbbrToReadable : Dict String String
+locMapAbbrToReadable =
+    fromList
+        [ ( "DESK", "Desk" )
+        , ( "OFFICE", "Office" )
+        , ( "CONFR", "Conference Room" )
+        , ( "COMMON", "Common Area" )
+        , ( "RESTROOM", "Restroom" )
+        , ( "PUBLIC", "Public Area" )
+        , ( "PRIVATE", "Private Area" )
+        , ( "MISC", "Miscellaneous" )
+        ]
+
+
+locMapReadableToAbbr : Dict String String
+locMapReadableToAbbr =
+    fromList
+        [ ( "Desk", "DESK" )
+        , ( "Office", "OFFICE" )
+        , ( "Conference Room", "CONFR" )
+        , ( "Common Area", "COMMON" )
+        , ( "Restroom", "RESTROOM" )
+        , ( "Public Area", "PUBLIC" )
+        , ( "Private Area", "PRIVATE" )
+        , ( "Miscellaneous", "MISC" )
+        ]
+
+
+getLocation : String -> Dict String String -> String
+getLocation loc locDict =
+    let
+        l =
+            get loc locDict
+    in
+        Maybe.withDefault "" l
+
+
+getLocationFromAbbr : String -> String
+getLocationFromAbbr loc =
+    getLocation loc locMapAbbrToReadable
+
+
+getLocationFromReadable : String -> String
+getLocationFromReadable loc =
+    getLocation loc locMapReadableToAbbr
