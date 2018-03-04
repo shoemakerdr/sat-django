31da09a03732121de0b05bbf3555b6d6bfc3b0f4
Derek Shoemaker <shoemakerdr@gmail.com>
2018-03-04 16:25:56 -0600

    Corrected config to have global elm variable

--- static/elm/webpack.config.js
+++ static/elm/webpack.config.js
@@ -8,15 +8,17 @@ const config = {
   output: {
     path: path.resolve(__dirname, "../js"),
     filename: "elm-bundle.js",
+    library: 'Elm'
   },
   cache: false,
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': '"production"'
     }),
-    new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin({
-      mangle: true,
+      mangle: {
+        reserved: ['Elm']
+      },
       compress: {
         warnings: false, // Suppress uglification warnings
         pure_getters: true,
@@ -30,7 +32,6 @@ const config = {
       exclude: [/\.min\.js$/gi] // skip pre-minified libs
     }),
     new webpack.IgnorePlugin(/^\.\/locale$/, [/moment$/]),
-    new webpack.NoErrorsPlugin(),
     new CompressionPlugin({
       asset: "[path].gz[query]",
       algorithm: "gzip",
