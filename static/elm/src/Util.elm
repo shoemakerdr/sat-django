b74cef811ea68bbc7b0e7a29d017ae386ac00337
Derek Shoemaker <shoemakerdr@gmail.com>
2018-03-18 01:46:59 -0500

    Added custom click handlers

--- static/elm/src/Util.elm
+++ static/elm/src/Util.elm
@@ -1,9 +1,38 @@
 module Util
     exposing
         ( (=>)
+        , onChange
+        , onClickWithPosition
         )
+
+import Json.Decode as Json exposing (Decoder)
+import Mouse exposing (Position)
+import Html exposing (Attribute)
+import Html.Events exposing (on, onWithOptions, targetValue)
 
 
 (=>) : a -> b -> ( a, b )
 (=>) =
     (,)
+
+
+onChange : (String -> msg) -> Attribute msg
+onChange tagger =
+    on "change" (Json.map tagger targetValue)
+
+
+onClickWithPosition : (Position -> msg) -> Attribute msg
+onClickWithPosition msg =
+    onWithOptions
+        "click"
+        { stopPropagation = True
+        , preventDefault = False
+        }
+        (Json.map msg positionDecoder)
+
+
+positionDecoder : Json.Decoder Position
+positionDecoder =
+    Json.map2 Position
+        (Json.field "pageX" Json.int)
+        (Json.field "pageY" Json.int)
