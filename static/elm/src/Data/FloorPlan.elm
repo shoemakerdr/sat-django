e44d7b4019b9f5bf3aa3e194848cc2e48be185c0
Derek Shoemaker <shoemakerdr@gmail.com>
2018-03-29 19:35:18 -0500

    Added new decoders and encoders.

--- static/elm/src/Data/FloorPlan.elm
+++ static/elm/src/Data/FloorPlan.elm
@@ -3,9 +3,14 @@ module Data.FloorPlan
         ( FloorPlan
         , Flag
         , floorplan
+        , encodeFloorplan
         )
 
-import Json.Decode as Json
+import Json.Decode as JD
+import Json.Encode as JE
+import Json.Decode.Pipeline as Pipeline
+import Data.Location exposing (Location, encodeLocations)
+import Util exposing ((=>))
 
 
 type alias FloorPlan =
@@ -22,7 +27,7 @@ type alias FloorPlan =
 
 
 type alias Flag a =
-    { a | locations : Json.Value }
+    { a | locations : JD.Value }
 
 
 floorplan : Flag FloorPlan -> FloorPlan
@@ -37,3 +42,43 @@ floorplan flag =
     , owner_name = flag.owner_name
     , last_updated = flag.last_updated
     }
+
+
+
+-- DECODERS
+
+
+decodeFloorplan : JD.Value -> Result String FloorPlan
+decodeFloorplan value =
+    JD.decodeValue floorplanDecoder value
+
+
+floorplanDecoder : JD.Decoder FloorPlan
+floorplanDecoder =
+    Pipeline.decode FloorPlan
+        |> Pipeline.required "id" JD.int
+        |> Pipeline.required "aspect_ratio" JD.float
+        |> Pipeline.required "image" JD.string
+        |> Pipeline.required "is_public" JD.bool
+        |> Pipeline.required "is_trashed" JD.bool
+        |> Pipeline.required "name" JD.string
+        |> Pipeline.required "owner" JD.int
+        |> Pipeline.required "owner_name" JD.string
+        |> Pipeline.required "last_updated" JD.string
+
+
+
+-- ENCODERS
+
+
+encodeFloorplan : FloorPlan -> List Location -> JE.Value
+encodeFloorplan floorplan locations =
+    JE.object
+        [ "id" => JE.int floorplan.id
+        , "is_public" => JE.bool floorplan.is_public
+        , "is_trashed" => JE.bool floorplan.is_trashed
+        , "name" => JE.string floorplan.name
+        , "owner" => JE.int floorplan.owner
+        , "owner_name" => JE.string floorplan.owner_name
+        , "locations" => encodeLocations locations
+        ]
