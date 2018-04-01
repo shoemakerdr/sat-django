a28b9337dc30f9b2b2eb7b05756eb394e9b1bdda
Derek Shoemaker <shoemakerdr@gmail.com>
2018-03-31 19:02:41 -0500

    Added initial version of FilterPanel module. Still working out how it will fit into entire application.

---
+++ static/elm/src/View/FilterPanel.elm
@@ -0,0 +1,192 @@
+module View.FilterPanel exposing (..)
+
+import Data.Filter as Filter exposing (Filter)
+import Html exposing (Html, div, h1, text, input, select, option, button, p)
+import Html.Attributes exposing (class, placeholder, value, selected)
+import Html.Events exposing (onInput, onClick)
+import Data.Location as Location exposing (Location)
+import Util exposing (onChange)
+
+
+-- MODEL
+
+
+type alias Model =
+    { filters : List (Filter FilterType Location)
+    , nameInput : String
+    , typeSelect : String
+    }
+
+
+type FilterType
+    = Name
+    | Type
+    | IsTrashed
+
+
+initialModel : Model
+initialModel =
+    { filters = [ isTrashedFilter ]
+    , nameInput = ""
+    , typeSelect = Location.noSelection
+    }
+
+
+isTrashedFilter : Filter FilterType Location
+isTrashedFilter =
+    (Filter.new IsTrashed (not << .is_trashed))
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NameInputChange String
+    | TypeSelectChange String
+    | ResetFilterForm
+
+
+type FilterMsg
+    = Merge
+    | Remove
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        NameInputChange name ->
+            let
+                filterMsg =
+                    case name of
+                        "" ->
+                            Remove
+
+                        _ ->
+                            Merge
+            in
+                updateFilter
+                    filterMsg
+                    (Filter.new Name (filterByName name))
+                    { model | nameInput = name }
+
+        TypeSelectChange locationType ->
+            let
+                filterMsg =
+                    if locationType == Location.noSelection then
+                        Remove
+                    else
+                        Merge
+            in
+                updateFilter
+                    filterMsg
+                    (Filter.new Type (filterByType locationType))
+                    { model | typeSelect = locationType }
+
+        ResetFilterForm ->
+            initialModel
+
+
+updateFilter : FilterMsg -> Filter FilterType Location -> Model -> Model
+updateFilter filterMsg filter model =
+    { model
+        | filters =
+            case filterMsg of
+                Remove ->
+                    Filter.remove filter model.filters
+
+                Merge ->
+                    Filter.merge filter model.filters
+    }
+
+
+filterByName : String -> Location -> Bool
+filterByName name location =
+    String.contains (String.toLower name) (String.toLower location.name)
+
+
+filterByType : String -> Location -> Bool
+filterByType locationType location =
+    let
+        locType =
+            Location.fromReadable locationType
+    in
+        locType == location.loc_type
+
+
+
+-- VIEW
+
+
+view : List Location -> Model -> Html Msg
+view locations model =
+    let
+        filteredLocations =
+            Filter.apply model.filters locations
+    in
+        div [ class "location-filter-wrapper" ]
+            [ h1 [ class "location-title" ] [ text "Locations" ]
+            , form model.nameInput model.typeSelect
+            , div [ class "location-list" ] <| locationInfoList locations
+            ]
+
+
+form : String -> String -> Html Msg
+form nameInput typeSelected =
+    div []
+        [ input
+            [ class "form-name-input"
+            , placeholder "Filter by name"
+            , value nameInput
+            , onInput NameInputChange
+            ]
+            []
+        , select [ class "form-select-type", onChange TypeSelectChange ] <| viewOptions typeSelected True
+        , button [ onClick ResetFilterForm ] [ text "Reset filter" ]
+        ]
+
+
+viewOptions : String -> Bool -> List (Html Msg)
+viewOptions typeSelected hasInitialOption =
+    let
+        opts =
+            Location.readableTypes
+
+        initialOption =
+            if hasInitialOption then
+                option
+                    [ selected <| typeSelected == Location.noSelection ]
+                    [ text Location.noSelection ]
+                    :: []
+            else
+                []
+    in
+        (++) initialOption
+            (opts
+                |> List.map
+                    (\opt ->
+                        option [ selected <| typeSelected == opt ] [ text opt ]
+                    )
+            )
+
+
+locationInfoList : List Location -> List (Html Msg)
+locationInfoList locations =
+    locations
+        |> List.map
+            (\location ->
+                let
+                    locationType =
+                        Location.fromAbbr location.loc_type
+
+                    extension =
+                        Location.extensionToString location.extension
+
+                    extString =
+                        if extension == "" then
+                            ""
+                        else
+                            ", ext. " ++ extension
+                in
+                    p [ class "location-list-info" ] [ text <| location.name ++ " - " ++ locationType ++ extString ]
+            )
