680a20ed18e357bf2511b1ef791f2bc891cb4c3c
Derek Shoemaker <shoemakerdr@gmail.com>
2018-04-02 14:42:02 -0500

    Added django-cors-headers

--- sat/settings.py
+++ sat/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'floorplans',
     'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
+# CORS config
+CORS_ORIGIN_WHITELIST = (
+    'localhost:8001'
+)
 
 ROOT_URLCONF = 'sat.urls'
 
